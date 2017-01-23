7561b1423b459912ea34c7fa44168ce9bb374f39
Alexa <alexa.anderson@outlook.com>
2017-01-23 17:33:41 -0600

    Form validation

--- components/quotes/quotesController.js
+++ components/quotes/quotesController.js
@@ -7,11 +7,15 @@ app.controller("QuotesCtrl",
 
       $scope.quotes = [];
 
-      $scope.createQuote = function(){
-        var quote = {};
-        quote.message = $scope.quoteParams.message;
-        quote.author = $scope.quoteParams.author;
-        $scope.quotes.push(quote);
+      $scope.createQuote = function(isValid, form){
+        if (isValid) {
+          var quote = {};
+          quote.message = $scope.quoteParams.message;
+          quote.author = $scope.quoteParams.author;
+          $scope.quotes.push(quote);
+        }
+        form.$setPristine();
+        form.$setUntouched();
       };
 
 }]);
