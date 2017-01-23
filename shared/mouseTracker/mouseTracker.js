ba08c81bf9b57a992dd3a74e4a573c0beff5e582
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-23 13:22:48 -0500

    Complete warmup mouse events

--- shared/mouseTracker/mouseTracker.js
+++ shared/mouseTracker/mouseTracker.js
@@ -2,20 +2,25 @@
 app.directive("mouseTracker", ["$window", function($window){
 
   var setHandlers = function(scope, element) {
-    angular.element("document").on("mouseup", function() {
-      angular.element(".mouse-position").text("UP");
+
+    angular.element(document).on("mouseup", function() {
+      angular.element(document.querySelector(".mouse-position")).text("UP");
     });
 
-    angular.element("document").on("mousedown", function() {
-      angular.element(".mouse-position").text("DOWN");
+    angular.element(document).on("mousedown", function() {
+      angular.element(document.querySelector(".mouse-position")).text("DOWN");
     });
 
-    angular.element(".mouse-hover").mouseenter(function() {
-      angular.element(".mouse-hover").text("MARTY MCFLY");
+    angular.element(document.querySelector(".mouse-tracker")).on("mouseenter", function() {
+      angular.element(document.querySelector(".mouse-hover")).text("HOVERIN'");
     });
 
-    angular.element(".mouse-hover").mouseleave(function() {
-      angular.element(".mouse-hover").text("OUT");
+    angular.element(document.querySelector(".mouse-tracker")).on("mouseleave", function() {
+      angular.element(document.querySelector(".mouse-hover")).text("AWAY");
+    });
+
+    angular.element(document.querySelector(".btn-info")).on("dblclick", function() {
+      angular.element(document.querySelector(".btn-info")).attr("value", "I'm dblclick");
     });
 
   };
