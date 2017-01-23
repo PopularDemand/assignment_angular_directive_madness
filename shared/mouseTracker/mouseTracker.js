54b74d7408d0c178b288b1a72db33d21de29003f
Alexa <alexa.anderson@outlook.com>
2017-01-23 12:05:43 -0600

    start mouse tracking

--- shared/mouseTracker/mouseTracker.js
+++ shared/mouseTracker/mouseTracker.js
@@ -1,10 +1,30 @@
 
-app.directive("mouseTracker", function(){
+app.directive("mouseTracker", ["$window", function($window){
+
+  var setHandlers = function(scope, element) {
+    angular.element("document").on("mouseup", function() {
+      angular.element(".mouse-position").text("UP");
+    });
+
+    angular.element("document").on("mousedown", function() {
+      angular.element(".mouse-position").text("DOWN");
+    });
+
+    angular.element(".mouse-hover").mouseenter(function() {
+      angular.element(".mouse-hover").text("MARTY MCFLY");
+    });
+
+    angular.element(".mouse-hover").mouseleave(function() {
+      angular.element(".mouse-hover").text("OUT");
+    });
+
+  };
 
   return {
     templateUrl: "shared/mouseTracker/mouseTracker.html",
     restrict: "E",
-    scope: {}
+    scope: {},
+    link: setHandlers
   };
 
-});
+}]);
